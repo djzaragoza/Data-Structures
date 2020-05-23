5764b53f52c44cfb607a739478adf775aa7fd6bb
unknown <dj.zaragoza@icloud.com>
2020-05-23 07:17:56 -0700

    finished debugging and copy pasted doubly linked list file to make queue file work

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,7 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
