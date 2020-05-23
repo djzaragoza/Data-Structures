54160ff549cbb08b54aa60563a0fa22f4b61ff4a
unknown <dj.zaragoza@icloud.com>
2020-05-23 11:58:05 -0700

    working on binary search tree file

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,6 +17,21 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        if value is None:
+            return
+        
+        elif self.value is None:
+            self.value = BinarySearchTree(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        else:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
         pass
 
     # Return True if the tree contains the value
